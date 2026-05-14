2488ca29ec4b5aaa23995ffa4c2daaa773986595
jameseduardo396-spec <jameseduardo396@gmail.com>
2026-05-14 12:22:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
-âge = int(input("Quel est ton âge ? "))
+from flask import Flask, request
+import os
 
-if âge >= 18:
-    print("Accès autorisé ! Bienvenue à la fête. 🎉🥳")
-else:
-    print("Désolé, tu es trop jeune. Reviens plus tard ! 🙁🔞")
+app = Flask(__name__)
+
+@app.route('/')
+def verifier_age():
+    age_url = request.args.get('age')
+    if age_url is None:
+        return "Veuillez ajouter votre âge à l'adresse (ex: ?age=20)"
+    try:
+        age = int(age_url)
+        if age >= 18:
+            return "Accès autorisé ! Bienvenue"
+        else:
+            return "Désolé, tu es trop jeune."
+    except:
+        return "L'âge doit être un nombre."
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+    
